b1e9a0978ef88b2950642dda48f6988c9aef9e94
sherlyc <sherlyc@gmail.com>
2018-07-01 19:21:42 +1200

    use graphql list type for collection

---
+++ index.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const { graphql, buildSchema } = require('graphql');
+
+const schema = buildSchema(`
+type Video {
+  id: ID,
+  title: String,
+  duration: Int,
+  watched: Boolean
+
+}
+type Query {
+  video: Video
+  videos: [Video]
+}
+
+type Schema {
+  query: Query
+}
+`);
+
+const videoA = {
+  id: 'a',
+  title: 'video A',
+  duration: 120,
+  watched: true
+}
+
+const videoB = {
+  id: 'b',
+  title: 'video B',
+  duration: 90,
+  watched: true
+}
+
+const videos = [videoA, videoB]
+
+const resolvers = {
+  video: () => ({
+    id: () => '1',
+    title: () => 'bar',
+    duration: () => 180,
+    watched: () => true,
+  }),
+  videos: () => videos,
+
+};
+
+const query = `
+query myFirstQuery {
+  videos {
+    id,
+    title,
+    duration,
+    watched
+  }
+}
+`;
+
+graphql(schema, query, resolvers)
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
+
+
