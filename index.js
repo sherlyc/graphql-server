c1d762e054ccb9955a6123a9fb794c87b943c3ed
sherlyc <sherlyc@gmail.com>
2018-07-01 19:38:55 +1200

    server graphql as middleware in express

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 'use strict';
 
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
 const { graphql, buildSchema } = require('graphql');
+
+const PORT = process.env.PORT || 3000;
+const server = express();
 
 const schema = buildSchema(`
 type Video {
@@ -47,19 +52,11 @@ const resolvers = {
 
 };
 
-const query = `
-query myFirstQuery {
-  videos {
-    id,
-    title,
-    duration,
-    watched
-  }
-}
-`;
+server.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true,
+}))
 
-graphql(schema, query, resolvers)
-  .then((result) => console.log(result))
-  .catch((error) => console.log(error));
-
-
+server.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+})
